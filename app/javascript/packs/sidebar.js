fa374f95d6bd8459fe2c37610ebaa7095c8c895e
KoichiEndo323 <waddledee323@gmail.com>
2023-07-16 06:42:00 +0000

    [fix]layout

--- app/javascript/packs/sidebar.js
+++ app/javascript/packs/sidebar.js
@@ -1,12 +1,21 @@
-// const li = document.querySelectorAll(".li");
-// console.log(li);
-// function activeLink() {
-//   li.forEach((item) =>
-//     item.classList.remove("active")
-//   );
-//   this.classList.add("active");
-// }
+const li = document.querySelectorAll(".navbar-menu li");
+console.log(li);
+function activeLink() {
+  li.forEach((item) =>
+    item.classList.remove("active")
+  );
+  this.classList.add("active");
+}
 
-// li.forEach((item) => {
-//   item.addEventListener("click", activeLink);
-// });
+li.forEach((item) => {
+  item.addEventListener("click", activeLink);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const navbarToggle = document.querySelector('.navbar-toggle');
+  const sidebar = document.querySelector('.sidebar');
+
+  navbarToggle.addEventListener('click', function() {
+    sidebar.classList.toggle('sidebar-open');
+  });
+});
